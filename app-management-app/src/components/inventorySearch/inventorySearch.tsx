3e2614a8c90689f516dde5630cd08bc32852b950
MariaMead <mmead@Academic.RRC.CA>
2026-01-15 14:49:00 -0600

    component for Inventory search with a custom reusable hook for state change called useSearchFilter. Tested to see if it renders seems to be working.

--- app-management-app/src/components/inventorySearch/inventorySearch.tsx
+++ app-management-app/src/components/inventorySearch/inventorySearch.tsx
@@ -1,3 +1,4 @@
+import { useSearchFilter } from "../../hooks/useSearchFilter";
 
 interface InventoryItem {
     id: string;
@@ -6,14 +7,34 @@ interface InventoryItem {
     quantity: number;
     price: number;
 }
-const inventory: InventoryItem[] = [];
+//Test data for Inventory list
+const dataInventory: InventoryItem[] = [
+{ id: "1", name: "Apple", category: "Fruit", quantity: 50, price: 0.5 },
+  { id: "2", name: "Banana", category: "Fruit", quantity: 30, price: 0.25 },
+  { id: "3", name: "Carrot", category: "Vegetable", quantity: 40, price: 0.3 },
+  { id: "4", name: "Broccoli", category: "Vegetable", quantity: 20, price: 0.75 },
+  { id: "5", name: "Chicken Breast", category: "Meat", quantity: 15, price: 5.0 }
+];
 
-// Function to look up inventory
-function InventorySearch({inventory}: {inventory: InventoryItem[]}) {
+
+// Function to filter inventory by text in a search field
+function InventorySearch({inventory = dataInventory}: {inventory?: InventoryItem[]}) {
+    //Setting state to prepare for input to change state used a custom hook called useSearch filter
+    const {search, setSearch, filteredText} = useSearchFilter(inventory, "name");
 
     return(
+        // Inventory section to show a table of inventory items
         <section className="inventory-search">
             <h2>Current Inventory</h2>
+            <label>
+                Inventory Search:
+                <input
+                    type="text"
+                    value={search}
+                    onChange={(text: React.ChangeEvent<HTMLInputElement>) =>
+                        setSearch(text.target.value)}
+                />    
+            </label>
 
             <table>
                 <thead>
@@ -27,7 +48,7 @@ function InventorySearch({inventory}: {inventory: InventoryItem[]}) {
                 </thead>
                     {/*Data for table will be populated here.*/}
                 <tbody className="inventoryTableBody">
-                    {inventory.map((item) => (
+                    {filteredText.map((item) => (
                         <tr key={item.id}>
                             <td>{item.id}</td>
                             <td>{item.name}</td>
