dfb65b10021990264bee9bcc1af7cd3bdbea95ae
Stacy Duguay <sduguay@rrc.ca>
2026-01-16 10:31:02 -0600

    Created low stock alert function within lowstockalerts.tsx and imported it within app.tsx.

--- app-management-app/src/components/LowStockAlerts.tsx
+++ app-management-app/src/components/LowStockAlerts.tsx
@@ -41,3 +41,50 @@ const mockInventory: InventoryItem[] = [
     lowStockThreshold: 5
   }
 ];
+
+function LowStockAlerts({
+  inventory = mockInventory
+}: {
+  inventory?: InventoryItem[];
+}) {
+  const lowStockItems = inventory.filter(
+    item => item.quantity <= item.lowStockThreshold
+  );
+
+    return (
+        <section className="low-stock-alerts">
+          <h2>Low stock Alert</h2>
+
+          {lowStockItems.length === 0? (
+            <p>All items are sufficiently stocked</p>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>Item Name</th>
+                  <th>Category</th>
+                  <th>Quantity</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+
+              <tbody className="low-stock-body">
+                {lowStockItems.map(item => (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.category}</td>
+                    <td>{item.quantity}</td>
+                    <td>
+                      {item.quantity === 0 ? "Out of Stock" : "Low Stock"}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </section>
+    );
+
+  }
+
+export default LowStockAlerts;
