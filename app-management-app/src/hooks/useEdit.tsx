cbb6acf6d5abe964a11d01caac10a96d16c76e48
MariaMead <mmead@Academic.RRC.ca>
2026-02-12 10:34:09 -0600

    Created a hook for the Quantity editor that is currently being used in lowStockAlert. I also added documentation to the hooks created.

--- app-management-app/src/hooks/useEdit.tsx
+++ app-management-app/src/hooks/useEdit.tsx
@@ -1,5 +1,22 @@
 import { useState } from "react";
 
+
+/**
+ * This is a custom hook that will handle editing in a component.
+ * This will handle the edit, save, cancel and onChange to update the state
+ * of the data being edited.
+ * The data is handled in temp data before its implemented as a change.
+ * @param initialData - Initial data to be edited. Object(Record<string, string>)
+ * @returns -{
+ *  isEditing: Is a boolean to check whether state is editing in edit mode. 
+ *  data: The confirmed/saved version of the data.
+ *  tempData: The "in-progress" draft data (bind this to your inputs).
+ *  handleEdit: Function to handle the data being edited sync temp data and current data.
+ *  handleSave: Function that handles the saving of the new data.
+ *  handCancel: Function that handles if the edit is canceled and discard temp data.
+ *  onChange: Handles the change of the previous data with changes made though edit.
+ * }
+ */
 export function useEdit(initialData: Record<string, string>) {
     const [isEditing, setIsEditing] = useState<boolean>(false);
     const [data, setData] = useState<Record<string, string>>(initialData);
@@ -17,20 +34,22 @@ export function useEdit(initialData: Record<string, string>) {
         setIsEditing(false);
     };
 
-    const handCancel = () => {
+    const handleCancel = () => {
         setTempData(data);
         setIsEditing(false);
     };
 
     const onChange = (field: string, value: string) => {
-        setTempData({...data, [field]: value});
+        setTempData(prev => ({...prev, [field]: value}));
     };
 
     return {
         isEditing,
+        data,
+        tempData,
         handleEdit,
         handleSave,
-        handCancel,
+        handleCancel,
         onChange
     }
 }
--- app-management-app/src/hooks/useFormInput.tsx
+++ app-management-app/src/hooks/useFormInput.tsx
@@ -5,8 +5,27 @@ type Validation = {
     error: string;
 }
 
-export function useFormInput() {
-    const [ value, setValue ] = useState<string>("");
+/**
+ * This is a custom hook, which will be used when we want to reuse presentation
+ * logic for a form. It will handle the errors presented to the user and call
+ * a form service method so that it can be used with different components,
+ * and have different behaviors.
+ * @param formServiceMethod - This is the service method will set the input validation
+ * behavior to check for errors.
+ * @param initialState - This is the initial state of the inputs.
+ * @returns - {
+ *  inputValue: The value of the input from the user.
+    setValue: The value set to update the state
+    error: The error to be displayed or null if valid.
+    onChange: Change handler to handle the input change from the user.
+    validateForm: Validate the form to test if inputs meet requirements.
+ * }
+ */
+export function useFormInput(
+    formServiceMethod: (initialValue: string | number) => Validation,
+    initialState : string | number = ""
+) {
+    const [ inputValue, setValue ] = useState<string | number>(initialState);
     const [ error, setError ] = useState<string | null>(null);
 
     const onchange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
@@ -15,14 +34,12 @@ export function useFormInput() {
     }
 
     // validate for error message
-    const validateFormError = (
-        valueValidator: (value: string) => string | null // to check if there is a error message or not
-    ): Validation => {
-        const errorMessage = valueValidator(value);
+    const validateForm = (): Validation => {
+        const errorMessage = formServiceMethod(inputValue);
 
         if(errorMessage) {
-            setError(errorMessage);
-            return { isValid: false, error: errorMessage};
+            setError(errorMessage.error);
+            return { isValid: false, error: errorMessage.error};
         }
 
         setError(null);
@@ -30,12 +47,11 @@ export function useFormInput() {
     }
 
     return {
-        value,
+        inputValue,
         setValue,
         onchange,
         error,
-        setError,
-        validateFormError
+        validateForm,
     }
     
 }
