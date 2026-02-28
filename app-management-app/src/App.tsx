55e1711c3813c7ebf4e764e1b31e7348ce7e28ba
Lindsay Encarnacion <lencarnacion@academic.rrc.ca>
2026-02-28 13:59:02 -0600

    removed all state and no props

--- app-management-app/src/App.tsx
+++ app-management-app/src/App.tsx
@@ -15,18 +15,14 @@ import NavInterface from './components/navInterface/navInterface';
 function App() {
   //initializes state with imported data
   // This is top level to be used in any child components
-  const [ inventory, setInventoryList ] = useState<InventoryItem[]>(dataInventory);
+  // const [ inventory, setInventoryList ] = useState<InventoryItem[]>(dataInventory);
 
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<NavInterface />} />
-        <Route path="/low-stock-alerts" element={<LowStockAlerts
-                                                   inventory={inventory}
-                                                   setInventoryList={setInventoryList}/>} />
-        <Route path="/inventory-search" element={<InventorySearch
-                                                   inventory={inventory}
-                                                   setInventoryList={setInventoryList}/>} />
+        <Route path="/low-stock-alerts" element={<LowStockAlerts />} />
+        <Route path="/inventory-search" element={<InventorySearch />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Route>
     </Routes>
