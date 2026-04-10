d1f034b6aae50039bc2b4c80f2b68bcbcfa927f3
MariaMead <mmead@Academic.RRC.ca>
2026-04-10 10:09:11 -0500

    Added authentication middleware, added it to the profile routes.

---
+++ app-management-app/apps/backend/src/api/v1/middleware/authentication.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { getAuth } from "@clerk/express";
+import * as profileService from "../services/profileService";
+import { FrontendProfile } from "@shared/types/frontend-profile";
+
+/**
+ * If a sessionToken is included in Authorization header, get userId from Clerk
+ * If user does not exist, add user to back-end database
+ */
+export const AuthenticateUser = async(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        // clerk getAuth method authenticates user against HTTP request Authorization heading
+        const auth = getAuth(req);
+        const userId = auth.userId;
+        
+        // store in simple userId table 
+        if(userId) {
+            let backendUser : FrontendProfile|null = await profileService.getProfileById(userId);
+            if(!backendUser) {
+                backendUser= await profileService.createProfile({id: userId});
+            }
+        }
+        
+        // If userId not found with auth, set userId to null 
+        // Prevents userId from being included erroneously in the request body
+        req.userId = userId;
+        next();
+    } catch(error) {
+        next(error);
+    }
+}
--- app-management-app/apps/backend/src/api/v1/routes/profileRoutes.ts
+++ app-management-app/apps/backend/src/api/v1/routes/profileRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { getProfile, updateProfile, getAllProfiles } from "../controllers/profileController";
+import { AuthenticateUser } from "../middleware/authentication";
+import { requireAdmin } from "../middleware/requireAdmin";
+import { requireAuth } from "@clerk/express";
 
 const router: Router = Router();
 
-router.get("/", getAllProfiles);
-router.get("/:id", getProfile);
-router.put("/:id", updateProfile);
+router.get("/", requireAuth, AuthenticateUser, requireAdmin, getAllProfiles);
+router.get("/:id", requireAuth, AuthenticateUser,getProfile);
+router.put("/:id", requireAuth, AuthenticateUser, updateProfile);
 
 export default router;
